10b2f9dda9f656b43cf957f144d9cf111ab3b47e
Divyaragukumar <114073642+Divyaragukumar@users.noreply.github.com>
2022-10-06 16:59:52 +0530

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <div className="section-title">
+        <h1>about us</h1>
+      </div>
+      <p>
+        rajesh the mass
+      </p>
+    </section>
+  );
+};
+
+export default About;
